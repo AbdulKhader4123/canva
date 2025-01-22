0971cad206367a53e736bd3bf48ac502f0d3622a
Consultant - Mohamed Thowfeeq Ahamad <consultant-mohamed.thowfeeq.ahamad@mcb.mu>
2025-01-22 15:19:54 +0530

    setting up module boundary

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -17,8 +17,82 @@ module.exports = [
           allow: ['^.*/eslint(\\.base)?\\.config\\.[cm]?js$'],
           depConstraints: [
             {
-              sourceTag: '*',
-              onlyDependOnLibsWithTags: ['*'],
+              sourceTag: 'type:app',
+              onlyDependOnLibsWithTags: [
+                'type:routes',
+                'type:api',
+                'type:feature',
+                'type:ui',
+                'type:data-access',
+                'type:utils',
+                'type:models',
+              ],
+            },
+            {
+              sourceTag: 'type:routes',
+              onlyDependOnLibsWithTags: [
+                'type:feature',
+                'type:data-access',
+                'type:utils',
+              ],
+            },
+            {
+              sourceTag: 'type:api',
+              onlyDependOnLibsWithTags: [
+                'type:feature',
+                'type:ui',
+                'type:data-access',
+                'type:utils',
+                'type:models',
+              ],
+            },
+            {
+              sourceTag: 'type:feature',
+              onlyDependOnLibsWithTags: [
+                'type:feature',
+                'type:ui',
+                'type:data-access',
+                'type:utils',
+                'type:api',
+                'type:models',
+              ],
+            },
+            {
+              sourceTag: 'type:ui',
+              onlyDependOnLibsWithTags: [
+                'type:ui',
+                'type:utils',
+                'type:models',
+              ],
+            },
+            {
+              sourceTag: 'type:data-access',
+              onlyDependOnLibsWithTags: [
+                'type:data-access',
+                'type:utils',
+                'type:api',
+                'type:models',
+              ],
+            },
+            {
+              sourceTag: 'type:utils',
+              onlyDependOnLibsWithTags: ['type:utils', 'type:models'],
+            },
+            {
+              sourceTag: 'type:models',
+              onlyDependOnLibsWithTags: ['type:models'],
+            },
+            {
+              sourceTag: 'domain:shared',
+              onlyDependOnLibsWithTags: ['domain:shared'],
+            },
+            {
+              sourceTag: 'domain:layout',
+              onlyDependOnLibsWithTags: ['domain:layout', 'domain:shared'],
+            },
+            {
+              sourceTag: 'domain:design',
+              onlyDependOnLibsWithTags: ['domain:design', 'domain:shared'],
             },
           ],
         },
